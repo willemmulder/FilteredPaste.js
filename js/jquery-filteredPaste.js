c23fb1cb0d9db940ae660e91880707149c821ccc
willemmulder <willemmulder@gmail.com>
2013-02-01 09:18:34 +0100

    Prevent scrolling

--- js/jquery-filteredPaste.js
+++ js/jquery-filteredPaste.js
@@ -44,7 +44,7 @@ $.filteredPaste = {
 };
 
 $(function() {
-	$("body").append("<div contenteditable='true' class='filteredPaste_pasteIntoArea' style='position: fixed; left: 10px; top: 10px; width: 1px; height: 1px; overflow: hidden;'>hi there</div>");
+	$("body").append("<div contenteditable='true' class='filteredPaste_pasteIntoArea' style='position: fixed; left: 50px; top: 50px; width: 1px; height: 1px; overflow: hidden;'>hi there</div>");
 });
 
 $.fn.filteredPaste = function(options) {
